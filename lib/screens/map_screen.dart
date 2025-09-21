4f3fbb8252171fccebe089f7cc2f2a9f7d8d67cb
Sepro <shtmdwns0@gmail.com>
2025-09-21 14:46:59 +0900

    refact: 가독성

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:geolocator/geolocator.dart';
