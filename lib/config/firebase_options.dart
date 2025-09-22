6e84e530345cff6ba1d4334d1bf987b6aab4195c
Sepro <shtmdwns0@gmail.com>
2025-09-22 14:34:30 +0900

    fix: env 수정

--- lib/config/firebase_options.dart
+++ lib/config/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,11 +53,11 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAkVyyjZCTacKAEmpob5C6lpxbHJLKn85Q',
-    appId: '1:955347166552:android:827bf4bbb6be1dbc57f12d',
-    messagingSenderId: '955347166552',
-    projectId: 'pedal-1e999',
-    storageBucket: 'pedal-1e999.firebasestorage.app',
+  static final FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+    appId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+    messagingSenderId: dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID']!,
+    storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET']!,
   );
 }
